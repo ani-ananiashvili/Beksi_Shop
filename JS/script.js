5c6cf34eff74fed332da804bb390b85314267d29
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-08-04 23:04:29 +0400

    changes

--- JS/script.js
+++ JS/script.js
@@ -14,3 +14,43 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+
+// registration
+
+const registrationIcon = document.getElementById('registrationIcon');
+  const tooltip = registrationIcon.querySelector('.tooltip');
+  const registrationOverlay = document.getElementById('registrationOverlay');
+  const closeFormButton = document.getElementById('closeForm');
+
+  registrationIcon.addEventListener('mouseover', () => {
+    tooltip.style.display = 'block';
+  });
+
+  registrationIcon.addEventListener('mouseout', () => {
+    tooltip.style.display = 'none';
+  });
+
+  registrationIcon.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    registrationOverlay.style.display = 'flex'; 
+  });
+
+  closeFormButton.addEventListener('click', () => {
+    registrationOverlay.style.display = 'none'; 
+  });
+
+
+
+// lighting animation & search 
+
+const videoContainer = document.querySelector('.video-container');
+const searchForm = document.getElementById('searchForm');
+
+videoContainer.addEventListener('mouseover', () => {
+    searchForm.style.display = 'block'; // Show form on hover
+});
+
+videoContainer.addEventListener('mouseout', () => {
+    searchForm.style.display = 'none'; // Hide form when not hovering
+});
