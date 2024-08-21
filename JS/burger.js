71ef19e60a366b2910bc5d15aed8dc797c354711
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-08-21 17:33:53 +0400

    fix burger-bar bug

---
+++ JS/burger.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const header = document.querySelector(".nav-bar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+
+  if (navMenu.classList.contains("active")) {
+    header.style.transform = "translateY(-100%)";
+  } else {
+    header.style.transform = "translateY(0)";
+  }
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    header.style.transform = "translateY(0)";
+  })
+);
